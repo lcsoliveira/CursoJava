fae17e38d3603ea72d68488b57c86d8f7e64ec99
lucasOliveiraa <java01@trn9.(none)>
2016-09-24 11:31:02 -0300

    ok

---
+++ Aula2/src/main/java/aula2/HelloWord.java
@@ -0,0 +1,13 @@
+package aula2;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("hello world");
+	}
+
+}
